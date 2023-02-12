d5516d36bedde69a854281ce52508a4225e5a128
Ayush Aman <ayush.aman2021@vitbhopal.ac.in>
2023-02-12 11:25:39 +0530

    Slight Modifications

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -4,15 +4,15 @@ from cleantext import clean
 from tqdm import tqdm
 from textblob import TextBlob, Word     # correct the words in the sentences 
 
-# stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 
-# 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 
-# 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did',
-# 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through',
-# 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 
-# 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too',
-# 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 
-# 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', 
-# "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
+stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 
+'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 
+'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did',
+'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through',
+'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 
+'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too',
+'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 
+'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', 
+"needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
 
 def stopwords_removal(sentence):
     word_tokens = sentence.split()
@@ -35,68 +35,34 @@ def remove_specialchar(sentence):
 
     return(new_string)
 
-data1 = pd.read_csv(r"4O0_-1NaWnY_5348_comments.csv") 
-#data2 = pd.read_csv(r"WHoWGNQRXb0_1187_comments.csv")                             
+data = pd.read_csv(r"4O0_-1NaWnY_5348_comments.csv") 
+                            
 
 
 total_operations = 4  # number of operations performed
 with tqdm(total=total_operations, desc='Training Data cleaning', unit='op') as pbar:
 
-    data1['Comment'] = data1['Comment'].str.lower()
+    data['Comment'] = data['Comment'].str.lower()
     pbar.update(1)
 
-    data1['Comment'] = data1['Comment'].apply(lambda x: correct_sentence_spelling(x))
+    data['Comment'] = data['Comment'].apply(lambda x: correct_sentence_spelling(x))
     pbar.update(1)
 
-    data1['Comment'] = data1['Comment'].apply(lambda x: remove_emoji(x))
+    data['Comment'] = data['Comment'].apply(lambda x: remove_emoji(x))
     pbar.update(1)
 
-    data1['Comment'] = data1['Comment'].apply(lambda x: remove_specialchar(x))
+    data['Comment'] = data['Comment'].apply(lambda x: remove_specialchar(x))
     pbar.update(1)
 
-    # data1['Comment'] = data1['Comment'].apply(lambda x : stopwords_removal(x))
-    # pbar.update(1)
+    data['Comment'] = data['Comment'].apply(lambda x : stopwords_removal(x))
+    pbar.update(1)
 
-    data1['polarity'] = data1['Comment'].apply(lambda x: TextBlob(x).sentiment.polarity)
-    data1['pol_cat']  = 0
-
-
-data1['pol_cat'][data1.polarity > 0] = 1
-#data['pol_cat'][data.polarity == 0] = 0
-data1['pol_cat'][data1.polarity <= 0] = -1
+    data['polarity'] = data['Comment'].apply(lambda x: TextBlob(x).sentiment.polarity)
+    data['pol_cat']  = 0
 
 
-# with tqdm(total=total_operations, desc='Test Data cleaning', unit='op') as pbar:
-#     data2['Comment'] = data2['Comment'].str.lower()
-#     pbar.update(1)
-
-#     data2['Comment'] = data2['Comment'].apply(lambda x: correct_sentence_spelling(x))
-#     pbar.update(1)
-
-#     data2['Comment'] = data2['Comment'].apply(lambda x: remove_emoji(x))
-#     pbar.update(1)
-
-#     data2['Comment'] = data2['Comment'].apply(lambda x: remove_specialchar(x))
-#     pbar.update(1)
-
-#     data2['Comment'] = data2['Comment'].apply(lambda x : stopwords_removal(x))
-#     pbar.update(1)
-    
-#     data2['polarity'] = data2['Comment'].apply(lambda x: TextBlob(x).sentiment.polarity)
-#     data2['pol_cat']  = 0
+data['pol_cat'][data.polarity > 0] = 1
+data['pol_cat'][data.polarity <= 0] = -1
 
 
-
-
-#data2['pol_cat'][data2.polarity > 0] = 1
-#data['pol_cat'][data.polarity == 0] = 0
-#data2['pol_cat'][data2.polarity <= 0] = -1
-
-
-data1.to_csv('Clean1_comments.csv')
-#data2.to_csv('Clean2_comments.csv')
-
-
-#print(data.head())
-#data.pol_cat.value_counts().plt.bar()
-#print(data.pol_cat.value_counts())
+data.to_csv('Cleaned_comments.csv')
--- src/train.py
+++ src/train.py
@@ -1,27 +1,14 @@
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-#from sklearn import metrics
-from sklearn.svm import SVC, LinearSVC
-from sklearn.naive_bayes import GaussianNB
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.neural_network import MLPClassifier
+from sklearn.svm import LinearSVC
 from sklearn.model_selection import cross_val_score
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import f1_score
 
-# Load the data
-data1 = pd.read_csv("Clean1_comments.csv")
-#data2 = pd.read_csv("Clean2_comments.csv")
-
-# Split the data into training and testing sets
-# train_data = data1
-# test_data = data2
+# Loading the data
+data = pd.read_csv("Cleaned_comments.csv")
 
 # Split the data into 80% training data and 20% testing data
-train_data, test_data, train_labels, test_labels = train_test_split(data1.drop("Likes", axis=1), data1["pol_cat"], test_size=0.1)
+train_data, test_data, train_labels, test_labels = train_test_split(data.drop("Likes", axis=1), data["pol_cat"], test_size=0.1)
 
 # Convert the comments to numerical feature vectors using the CountVectorizer
 vectorizer = CountVectorizer()
@@ -30,41 +17,15 @@ train_features = vectorizer.fit_transform(train_data['Comment'])
 test_data = test_data.dropna()
 test_features = vectorizer.transform(test_data['Comment'])
 
-# Train a logistic regression model on the training data
-# model = LogisticRegression()
-# model.fit(train_features, train_data["pol_cat"])
+
+model = LinearSVC()
+model.fit(train_features, train_data["pol_cat"])
 
 
-# model = RandomForestClassifier()
-# model.fit(train_features, train_data["pol_cat"])
-
-models = [
-    RandomForestClassifier(),
-    SVC(),
-    GaussianNB(),
-    KNeighborsClassifier(),
-    MLPClassifier(),
-    LinearSVC(),
-    LogisticRegression()
-]
-
-train_features = train_features.toarray()
-
-# Predict the sentiment of the comments in the testing set
-# predictions = model.predict(test_features)
-
-for model in models:
+for i in range(0, 100):
     scores = cross_val_score(model, train_features, train_data["pol_cat"], cv=5)
-    print(f"Accuracy of {model.__class__.__name__}: {scores.mean()}")
-
-# Calculate the accuracy of the model
-# accuracy = accuracy_score(test_data["pol_cat"], predictions)
-# print("Accuracy:", accuracy)
 
 
-# f1 = f1_score(test_data["pol_cat"], predictions, average='weighted')
-# print("F1 Score:", f1)
+print(f"Accuracy of {model.__class__.__name__}: {scores.mean()}")
 
 
-# Load the dataset into a pandas DataFrame
-#df = pd.read_csv("dataset.csv")
